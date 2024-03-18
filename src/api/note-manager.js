5ea83a4edec9cc4be9c80c999257cbe2b88f900c
DP500KPREDA10\greta-cfa <forma.dvlp@mailo.com>
2024-03-18 13:23:52 +0100

    add HEADERS_API create and remove

--- src/api/note-manager.js
+++ src/api/note-manager.js
@@ -22,7 +22,11 @@ export class NoteManager {
   static async create(note) {
     const response = await fetch(BASE_API_URL, {
       method: "POST",
-      // headers: HEADERS_API,
+      // headers: HEADERS_API
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": NoteManager.token
+      },
       body: JSON.stringify(note)
     });
     const data = await response.json();
@@ -33,6 +37,10 @@ export class NoteManager {
     const response = await fetch(BASE_API_URL + id, {
       method: "DELETE",
       // headers: HEADERS_API
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": NoteManager.token
+      }
     });
     const data = await response.json();
     return data;
